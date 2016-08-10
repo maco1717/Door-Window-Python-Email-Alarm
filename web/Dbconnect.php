2807a5db6ae65d5bc307770e60539b848b150deb
root <root@SmartDoor.home>
2016-08-10 23:52:59 +0100

    Updates

--- web/Dbconnect.php
+++ web/Dbconnect.php
@@ -1,4 +1,4 @@
-<?php
+  <?php
 //connect to mysql database
 $con = mysqli_connect("localhost", "marco", "password", "login") or die("Error " . mysqli_error($con));
 
--- web/Login.php
+++ web/Login.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 
-
-
 include_once 'Dbconnect.php';
 
 //check if form is submitted
