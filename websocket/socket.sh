667eb15a2041b1beccef92530510e302c2cdee89
root <root@SmartDoor.home>
2016-08-10 23:01:25 +0100

    Updates

--- websocket/socket.sh
+++ websocket/socket.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-#variables#
-#Files
-#email_alarm_status var location
-#log location
-#log events
-#json data
-#python "server" location
-#websocket applicatio n location
-#socket.sh location for ((COUNT = 1; COUNT <=10; COUNT++))
+source EmailDoorAlarm.conf
 
 while true
 do
-  var=$(</home/pi/var/var)
+  var=$(<$path_email_alarm_status)
   echo "$var"
   read -t 0.01 input
   input=${input:-$var}
   if [ "$input" != "$var" ]
   then
-    echo "$input" > "/home/pi/var/var"
+    echo "$input" > "$path_email_alarm_status"
   fi
 done
